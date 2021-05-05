763575a119dcf4bfa01d065ef414ad41e9875483
Alan <dittler.a@gmail.com>
2021-05-05 18:31:06 -0300

    first-version

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,32 @@
+<template>
+  <select
+    @change="onChange"
+    v-model="selected"
+    class="form-select bg-gray-300 bg-opacity-25 block w-full border p-3 rounded mt-10"
+  >
+    <option value="0">select</option>
+    <option v-for="country in countries" :key="country.ID" :value="country.ID">
+      {{ country.Country }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "CountrySelect",
+  props: ["countries"],
+  data() {
+    return {
+      selected: 0,
+    };
+  },
+  methods: {
+    onChange() {
+      const country = this.countries.find((item) => item.ID === this.selected);
+      this.$emit("get-country", country);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+    <CountrySelect :countries="countries" @get-country="getCountryData" />
+    <button
+      v-if="stats.Country"
+      @click="clearCountryData"
+      class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"
+    >
+      Clear Country
+    </button>
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-grey-500 text-3xl mt-10 mb6">Fetching Data</div>
+
+    <img :src="loadingImage" class="w-24 m-auto" alt="loading" />
+  </main>
+</template>
+
+<script>
+import DataTitle from "../components/DataTitle";
+import DataBoxes from "../components/DataBoxes";
+import CountrySelect from "../components/CountrySelect";
+
+export default {
+  name: "Home",
+  components: { DataTitle, DataBoxes, CountrySelect },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/tenor.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = "Global";
+      this.stats = data.Global;
+      this.loading = false;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
+};
+</script>
